ad03a49811ea54d3ab5549e3fa362fe8141d44b3
VadimA <wadimanosov@gmail.com>
2018-03-16 21:01:36 +0400

    Added implementation for PersonController

--- src/main/java/org/neo/mint/controller/PersonController.java
+++ src/main/java/org/neo/mint/controller/PersonController.java
@@ -1,8 +1,13 @@
 package org.neo.mint.controller;
 
 import org.neo.mint.db.PersonRepository;
+import org.neo.mint.db.domain.Person;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 /**
  * Created by vanosov on 16.03.2018.
@@ -15,5 +20,28 @@ public class PersonController {
     @Autowired
     public PersonController(PersonRepository personRepository){
         this.personRepository = personRepository;
+    }
+
+    @GetMapping("/person/{name}")
+    public ResponseEntity<Person> getPerson(@PathVariable String name){
+        Optional<Person> person = personRepository.findById(name);
+
+        if(person.isPresent()){
+            return ResponseEntity.ok(person.get());
+        } else{
+            return ResponseEntity.badRequest().body(null);
+        }
+    }
+
+    @PostMapping("/person")
+    public ResponseEntity<HttpStatus> savePerson(@RequestBody Person person) {
+        personRepository.save(person);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/person/{name}")
+    public ResponseEntity<HttpStatus> deletePerson(@PathVariable String name) {
+        personRepository.delete(name);
+        return ResponseEntity.noContent().build();
     }
 }
