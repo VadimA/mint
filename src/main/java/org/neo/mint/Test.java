0ffed80c73e7f972c85e83c57e5fdf9706c12faf
VadimA <wadimanosov@gmail.com>
2018-03-28 20:47:42 +0400

    Added authorization with Okta service

--- src/main/java/org/neo/mint/Test.java
+++ src/main/java/org/neo/mint/Test.java
@@ -4,7 +4,11 @@ import java.io.*;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
+
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * Created by vanosov on 20.03.2018.
@@ -12,15 +16,20 @@ import java.util.stream.Collectors;
 public class Test {
 
         public static void main(String[] args)  throws IOException {
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            StringBuilder text = new StringBuilder();
-            String line;
-            while( (line = br.readLine()) != null) {
-                text.append(line);
+            //BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            String str = "word\ncount\ncat\nword\ncount2\ncat2\nword\ncount2\ncat2";
+            String [] input = (Stream.of(str).collect(Collectors.joining(" "))).split("\n");
+            String currentKey = input[0];
+            Integer currentCount = 1;
+            for(int i = 1; i < input.length; i++){
+                if(!currentKey.equals(input[i])){
+                    System.out.println(currentKey + '\t' + currentCount);
+                    currentKey = input[i];
+                    currentCount = 1;
+                }
+                else{
+                    currentCount++;
+                }
             }
-            String [] input = text.toString().split(" ");
-            for(int i = 0; i < input.length-1; i++){
-                System.out.println(input[i] + "\t1");
-            }
-    }
+        }
 }
